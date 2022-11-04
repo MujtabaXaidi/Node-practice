3af8c6c51d9dfdbca2afb124104b869f8c6edc8e
Mujtaba <mujtabaabbasx@gmail.com>
2022-11-04 15:08:37 +0500

    Promise/Blocking code

--- 13-Eventloop.js
+++ 13-Eventloop.js
@@ -1,5 +1,10 @@
-console.log('first')
-setTimeout(()=>{
-  console.log('second')
-},1000)
-console.log('third')
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+  console.log('request event')
+  res.end('Hello World')
+})
+
+server.listen(5000,()=>{
+  console.log('Server listening on port : 5000')
+})
